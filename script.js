96f9c3d1f509f108bdbf960a7ded4da45ce648d2
Egor <JackDenielsYT@gmail.com>
2024-11-30 22:55:19 +1000

    30.11.2024

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 // THEME
 
-var theme = document.getElementById('themeToggle');
-var body = document.querySelector('body');
+var theme = document.getElementById('themeToggle')
+
+var body = document.querySelector('body')
+
+var h1 = document.getElementById('h1')
+var hr = document.getElementById('hr')
 
 theme.addEventListener('click', function() {
     if (theme.style.color == 'white') {
         theme.style.color = 'black'
-        theme.innerText = 'Light Theme'
-
-        body.style.backgroundColor = 'white';
-    } else {
-		theme.style.color = 'white'
         theme.innerText = 'Dark Theme'
 
+        body.style.backgroundColor = 'white'
+
+        h1.style.color = 'black'
+
+        hr.style.borderTopColor = 'black'
+    } else {
+		theme.style.color = 'white'
+        theme.innerText = 'Light Theme'
+
 		body.style.backgroundColor = 'black'
+
+        h1.style.color = 'white'
+
+        hr.style.borderTopColor = 'white'
 	}
 })
