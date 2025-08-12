ab9c2659dd7c57146528f7b4e3d4cef571ad7ffd
Egor <JackDenielsYT@gmail.com>
2025-08-12 13:55:12 +1000

    12/08/2025 13/55

--- example-2/script.js
+++ example-2/script.js
@@ -24,34 +24,3 @@ toggleTheme.addEventListener('click', function() {
 		sideMenu.style.transition = '1.5s'
 	}
 })
-
-
-// LANGUAGE
-var Language = document.getElementById('Language')
-
-var aboutComplect = document.getElementById('aboutComplect')
-var aboutCompany = document.getElementById('aboutCompany')
-var news = document.getElementById('news')
-var mediaBank = document.getElementById('mediaBank')
-var contacts = document.getElementById('contacts')
-
-Language.addEventListener('click', function() {
-    if (this.getAttribute('data-show') === 'true') {
-		this.innerText = 'English'
-		this.setAttribute('data-show', 'false')
-        aboutComplect.innerText = 'О комплекте'
-        aboutCompany.innerText = 'О компании'
-        news.innerText = 'Новости'
-        mediaBank.innerText = 'Медиа Банк'
-		contacts.innerText = 'Контакты'
-	} else {
-		this.innerText = 'Русский'
-		this.setAttribute('data-show', 'true')
-        aboutComplect.innerText = 'About complect'
-        aboutCompany.innerText = 'About company'
-        news.innerText = 'News'
-        mediaBank.innerText = 'Media Bank'
-		contacts.innerText = 'Сontacts'
-	}
-})
-
