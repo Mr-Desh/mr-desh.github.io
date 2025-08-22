d9143010e3a367a7e7cc5213c6d17664f2f01dd2
Mr-Desh <jackdenielsyt@gmail.com>
2025-08-22 18:29:22 +1000

    22/08/2025 18/29

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
-"use strict"
+"use strict";
 
 // THEME
 
-let theme = document.getElementById('themeToggle')
-let body = document.querySelector('body')
-let h1 = document.querySelector('h1')
-let blocks = document.querySelectorAll('.main__example-work-block')
-let a = document.querySelectorAll('a')
-let hr = document.querySelector('hr')
+let theme = document.getElementById("themeToggle");
+let h1 = document.querySelector("h1");
+let blocks = document.querySelectorAll(".main__example-work-block");
+let a = document.querySelectorAll("a");
+let hr = document.querySelector("hr");
 
-theme.addEventListener('click', function() {
-    if (theme.innerText == 'Light Theme') {
-        theme.innerText = 'Dark Theme'
-        theme.style.color = 'black'        
-        body.style.backgroundColor = 'white'
-        h1.style.color = 'black'
-        hr.style.border = '5px solid black'
-        a.forEach(a => 
-            {
-                a.style.color = 'black'
-            }
-        );
-        blocks.forEach(blocks => 
-            {
-                blocks.style.border = '3px solid black'
-            }
-        )
+theme.addEventListener("click", function () {
+    if (theme.innerText == "Light Theme") {
+        theme.innerText = "Dark Theme";
+        theme.style.color = "black";
+        document.body.style.backgroundColor = "white";
+        h1.style.color = "black";
+        hr.style.border = "5px solid black";
+        a.forEach((a) => {
+            a.style.color = "black";
+        });
+        blocks.forEach((blocks) => {
+            blocks.style.border = "3px solid black";
+        });
     } else {
-		theme.innerText = 'Light Theme'
-        theme.style.color = 'white'
-		body.style.backgroundColor = 'black'
-        h1.style.color = 'white'
-        hr.style.border = '5px solid white'
-        a.forEach(a =>
-            {
-                a.style.color = 'white'
-            }
-        );
-        blocks.forEach(blocks =>
-            {
-                blocks.style.border = '3px solid white'
-            }
-        )
-	}
-})
+        theme.innerText = "Light Theme";
+        theme.style.color = "white";
+        document.body.style.backgroundColor = "black";
+        h1.style.color = "white";
+        hr.style.border = "5px solid white";
+        a.forEach((a) => {
+            a.style.color = "white";
+        });
+        blocks.forEach((blocks) => {
+            blocks.style.border = "3px solid white";
+        });
+    }
+});
 
 // Modals Windows
 
-let block_4_for_modal = document.getElementById('block-4-for-modal')
+let block_4_for_modal = document.getElementById("block-4-for-modal");
 
-let myModal_4 = document.getElementById('myModal-4')
-let myModal_4_Close = document.getElementById('myModal-4-Close')
+let myModal_4 = document.getElementById("myModal-4");
+let myModal_4_Close = document.getElementById("myModal-4-Close");
 
-block_4_for_modal.addEventListener('click', function() {
-    myModal_4.style.display = 'block'    
-})
+block_4_for_modal.addEventListener("click", function () {
+    myModal_4.style.display = "block";
+});
 
-myModal_4_Close.addEventListener('click', function() {
-    myModal_4.style.display = 'none'
-})
+myModal_4_Close.addEventListener("click", function () {
+    myModal_4.style.display = "none";
+});
