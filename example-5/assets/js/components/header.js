7b881f3efdce4186f1ed78ce596579d685a43058
Mr-Desh <jackdenielsyt@gmail.com>
2025-08-28 18:41:54 +1000

    28/08/2025 18/41

--- example-5/assets/js/components/header.js
+++ example-5/assets/js/components/header.js
@@ -15,17 +15,41 @@ async function loadHeader() {
 
         // Инициализация
         initBackToPortfolio();
+        initNavigation();
     } catch (error) {
         console.log("Header not load!");
     }
 }
 
+// Вернуться к портфолио
 function initBackToPortfolio() {
     const BackToPortfolio = document.getElementById("portfolioLink");
     if (BackToPortfolio) {
         BackToPortfolio.addEventListener('click', () => {
             window.location.href = '../../../index.html';
         });
+    }
+}
+
+// Навигая
+function initNavigation() {
+    const logo = document.getElementById('logo');
+    if (logo) {
+        logo.addEventListener('click', () => {
+            window.location.href = '../home/index.html';
+        })
+    }
+    const home = document.getElementById('home');
+    if (home) {
+        home.addEventListener('click', () => {
+            window.location.href = '../home/index.html';
+        })
+    }
+    const domains = document.getElementById('domains');
+    if (domains) {
+        domains.addEventListener('click', () => {
+            window.location.href = '../domains/index.html';
+        })
     }
 }
 
